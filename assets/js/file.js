8f3e2251121d423d86a5537e0fdd18baacafa63a
Davide Siracusano <152174921+DavideSiracusano@users.noreply.github.com>
2024-01-21 16:05:39 +0100

    Add files via upload

--- assets/js/file.js
+++ assets/js/file.js
@@ -1,26 +1,47 @@
 let counter = 0;
-let decreaseNumber = document.getElementById("decrease-number");
-let increaseNumber = document.getElementById("increase-number");
-let startNumber = document.getElementById("start-number");
+
+const h1 = document.createElement("h1");
+h1.setAttribute("id", "counter");
+h1.textContent = "Counter";
+document.body.appendChild(h1);
+
+const h2 = document.createElement("h2");
+h2.setAttribute("id", "start-number");
+h2.textContent = 0;
+document.body.appendChild(h2);
+
+const div = document.createElement("div");
+div.setAttribute("id", "container");
+document.body.append(div);
+
+const button1 = document.createElement("button");
+button1.setAttribute("id", "decrease-number");
+button1.textContent = "-";
+div.appendChild(button1);
+
+const button2 = document.createElement("button");
+button2.setAttribute("id", "increase-number");
+button2.textContent = "+";
+div.appendChild(button2);
 
 function minus() {
   if (counter <= 0) {
     return;
   } else {
     counter--;
-    startNumber.textContent = counter;
+    h2.textContent = counter;
   }
 }
 
 function plus() {
   if (counter === 49) {
     counter++;
-    startNumber.textContent = "your patience is your power!";
+    h2.textContent = "your patience is your power!";
   } else {
     counter++;
-    startNumber.textContent = counter;
+    h2.textContent = counter;
   }
 }
 
-decreaseNumber.addEventListener("click", minus);
-increaseNumber.addEventListener("click", plus);
+button1.addEventListener("click", minus);
+button2.addEventListener("click", plus);
